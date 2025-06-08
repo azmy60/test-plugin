b7b0d89ce1e45dce7764a55dd6ca7c9f92d5821a
azmy60 <mazmiagus2003@gmail.com>
2025-06-08 16:38:31 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// To use the @beekeeperstudio/plugin:
+// 1. `yarn install` or `npm install`
+// 2. Restart your Beekeeper Studio or click [DEV] Reload Plugin View
+//
+// import "./node_modules/@beekeeperstudio/plugin/dist/eventForwarder.js"; // IMPORTANT! Enables keyboard/mouse event forwarding from iframe to parent
+// import {
+//     request,
+//     addNotificationListener,
+// } from "./node_modules/@beekeeperstudio/plugin/dist/index.js";
+
+console.log("The plugin javascript is working too!");
